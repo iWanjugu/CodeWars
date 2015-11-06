66fcf22b50f5cdc285acf7453571b35c9bacac31
Wanjugu Irene <ireneemaina@gmail.com>
2015-11-06 10:46:41 +0300

    Grid blast changes

--- Python/grid_blast.py
+++ Python/grid_blast.py
@@ -20,4 +20,39 @@ __author__ = 'wanjugu'
 #Creating matrices
 
 from numpy import array
-A = array
+import numpy as np #for matrix/array creation
+A = array(([3, 8, 5],\
+           [6, 4, 7]))
+
+# print (A)
+
+x = np.arange(10)
+print (x)
+
+#You can index as usual
+# print(x[2])
+
+#multidimnsional arrays
+x.shape = (2,5)
+print (x)
+print (x[0]) #prints the first row
+print (x[1]) #prints the second row
+
+print (x[1,3]) #prints item at index [3] in the second row [1]
+
+# Example
+k = np.arange(10)
+key_num = k[1:]
+print(key_num)
+
+key_num.shape = (3,3)
+print(key_num)
+
+grid = array ((["top left", "top middle", "top right"],\
+              ["middle left", "center middle", "center right"],\
+              ["bottom left", "bottom middle", "bottom right"]))
+
+print (grid)
+
+
+
